92fc8009870c971d3e9eedca907308b3eca72a6e
PrestonRoberts <roberts.preston123@gmail.com>
2018-11-13 14:39:59 -0500

    First Commit

--- app/models.py
+++ app/models.py
@@ -1,16 +1,4 @@
-from app import db, login
+from app import db
 from flask_login import UserMixin
 
 
-@login.user_loader
-def load_user(id):
-    return Player.query.get(int(id))
-
-
-class Player(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(16))
-    email = db.Column(db.String(45))
-
-    def __repr__(self):
-        return '<Player {}>'.format(self.name)
