eb10a3aea2becde9391ccb206a2553f88dfdb74d
Preston Roberts <40707604+PrestonRoberts@users.noreply.github.com>
2018-11-27 14:18:13 -0500

    Add files via upload

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
-from app import app
+from app import app, db
 from app.forms import LoginForm
+from app.models import Player
 
 
 @app.route('/')
@@ -26,4 +27,14 @@ def home():
 
 @app.route('/leaderboard')
 def leaderboard():
-    return render_template('leaderboard.html', title='Home')
+    players = Player.query.all()
+    list_size = 0
+    top_correct_guesses = []
+    while list_size < 10:
+        top_player = Player.query.filter_by(id=1).first()
+        for player in players:
+            if player.totalCorrectGuesses > top_player and player not in top_correct_guesses:
+                top_player = player
+        top_correct_guesses.append(top_player)
+        list_size += 1
+    return render_template('leaderboard.html', top_correct_guesses=top_correct_guesses, title='Home')
