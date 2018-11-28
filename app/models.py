50af4cdf35f289478014e7f7280fcc0df66ea75e
PrestonRoberts <roberts.preston123@gmail.com>
2018-11-28 10:07:05 -0500

    fixing something i did

--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
-from app import db, login
+from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-
+'''
 @login.user_loader
 def load_player(id):
     return Player.query.get(int(id))
+'''
 
 
 class Player(UserMixin, db.Model):
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app import app, db
 from app.forms import LoginForm, RoomForm
-from app. models import Player, PlayerToGame, GameRoom, Song, SongToGame, ChatMessage
+from app.models import Player, PlayerToGame, GameRoom, Song, SongToGame, ChatMessage
 
 
 @app.route('/')
@@ -27,7 +27,18 @@ def home():
 
 @app.route('/leaderboard')
 def leaderboard():
-    return render_template('leaderboard.html', title='Home')
+    players = Player.query.all()
+    list_size = 0
+    top_correct_guesses = []
+    while list_size < 10:
+        top_player = Player.query.filter_by(id=1).first()
+        for player in players:
+            if player.totalCorrectGuesses > top_player and player not in top_correct_guesses:
+                top_player = player
+        top_correct_guesses.append(top_player)
+        list_size += 1
+    return render_template('leaderboard.html', top_correct_guesses=top_correct_guesses, title='Home')
+
 
 @app.route('/create_room')
 def create_room():
