ec7b5cb9e2423cae4880fad6331c44f37d162541
PrestonRoberts <roberts.preston123@gmail.com>
2018-12-16 23:40:52 -0500

    changes

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -82,7 +82,25 @@ $(document).ready(function () {
         $('#myMessage').val('');
     }
 
-    var socket = io.connect(location.protocol + "//" + document.domain + ":" + location.port + "/" );
+    const server = express()
+      .use((req, res) => res.sendFile(INDEX) )
+      .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+    const io = socketIO(server);
+
+    io.on('connection', (socket) => {
+      console.log('Client connected');
+      socket.on('disconnect', () => console.log('Client disconnected'));
+    });
+
+    var socket = io();
+        var el = document.getElementById('server-time');
+
+        socket.on('time', function(timeString) {
+          el.innerHTML = 'Server time: ' + timeString;
+        });
+
+    //var socket = io.connect(location.protocol + "//" + document.domain + ":" + location.port + "/" );
 
     socket.on('connect', function(){
     });
